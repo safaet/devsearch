fa63df377267bf264f1cb2660777269fc3e3bd01
safaet <safaetjaman@gmail.com>
2023-07-22 15:33:15 +0600

    Signals

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     'projects.apps.ProjectsConfig',
     'users.apps.UsersConfig',
+
+    
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import User
 import uuid
 # Create your models here.
 
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null = True)
     username = models.CharField(max_length=200, blank=True, null=True)
@@ -23,7 +29,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
     
 
 
@@ -38,3 +44,5 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+    
+
