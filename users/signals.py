8dbce539f7a7ca6d1b6bd41f7fa81fb3ed114d63
safaet <safaetjaman@gmail.com>
2023-08-02 18:25:24 +0600

    User Edit Profile

--- users/signals.py
+++ users/signals.py
@@ -16,6 +16,16 @@ def createProfile(sender, instance, created, **kwargs):
             name = user.first_name,
         )
 
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
 
 def deleteUser(sender, instance, **kwars):
     user = instance.user
@@ -23,4 +33,5 @@ def deleteUser(sender, instance, **kwars):
 
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
